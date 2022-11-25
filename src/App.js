cc7e61eaf3fe5315ca8f570fafa714b3bbc3b70f
yoonjong-park <yoonjongpark@gmail.com>
2022-11-25 09:19:53 +0900

    #1 How to use Theme

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const roationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px; 
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  width: 100vh;
+  height: 100vh;
   justify-content: center;
   align-items: center;
-  animation: ${roationAnimation} 1s linear infinite;
-  ${Emoji} {  /* 스타일 컴포넌트 자체를 조작 : 가장 쿨한 부분. */
-    font-size: 36px;
-    &:hover {
-      /* & = span*/
-      font-size: 98px;
-
-    }
-    &:active {
-      opacity: 0;
-
-  }
+  background-color: ${props => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😃</Emoji>
-      </Box>
-      <Emoji>💗</Emoji>
+      <Title>goodJob</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components"; // Provider : 제공자
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
